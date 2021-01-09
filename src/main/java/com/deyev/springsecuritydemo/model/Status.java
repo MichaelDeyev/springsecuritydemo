5484c115e05d7428285925555873d72c1866775e
Michael Deyev <michael.deyev@gmail.com>
2021-01-09 16:22:46 +0300

    migration from inmemory to postgreSQL with JPA source

---
+++ src/main/java/com/deyev/springsecuritydemo/model/Status.java
@@ -0,0 +1,5 @@
+package com.deyev.springsecuritydemo.model;
+
+public enum Status {
+    ACTIVE, BANNED
+}
---
+++ src/main/java/com/deyev/springsecuritydemo/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.deyev.springsecuritydemo.security;
+
+import com.deyev.springsecuritydemo.model.User;
+import com.deyev.springsecuritydemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsServiceImpl")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User does not exist"));
+        return SecurityUser.fromUser(user);
+    }
+}
